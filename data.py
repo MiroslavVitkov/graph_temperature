fa902bf8b43542d57dc28afe091575a8e8cb3d62
Miroslav Vitkov <sir.vorac@gmail.com>
2013-08-16 22:40:30 +0300

    Abstract data holder class.

--- data.py
+++ data.py
@@ -1,12 +1,34 @@
 #!/bin/python
 
-import numpy as np
+"""Holds static x-axes for different ranges, as well as all the accumulated temperature data ever."""
 
-XAXIS = range(100)  # seconds
+# x axes in seconds for the different graphs
+X_MIN = range(60)
+X_HOUR = range(0, 60*60, 60)
+X_DAY = range(0, 60*60*24, 60*60)
+X_WEEK = range(0, 60*60*24*7, 60*60*24)
+X_MONTH = range(0, 60*60*24*7*4, 60*60*24*7)
+X_YEAR = range(0, 60*60*24*7*4*12, 60*60*24*7*4)
 
-import math
-y = np.sin(XAXIS)
-from plot import Plot
-p = Plot(XAXIS)
-p.update_fig(y)
- 
+class Data(object):
+    """General data structuring node. Can be associated with a file in r/rw/w/None way.
+
+    """
+    def __init__(self, datapoints, prev_node=None, next_node=None file=None, file_mode=None)
+        # Doubly linked list
+        self.prev_node = prev_node
+        self,next_node = next_node
+
+        # File handle
+        self.file = file
+        self.file_mode = file_mode
+
+        # Initialize all memoty to prevent memory allocation at runtime
+        self.temperatures = [0, ] * datapoints
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()  
