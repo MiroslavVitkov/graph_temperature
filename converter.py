b48f34f168a6cbe93baad50b2a09c57aa9e5152a
Miroslav Vitkov <sir.vorac@gmail.com>
2013-08-18 11:20:00 +0300

    Reworked the convertor

--- converter.py
+++ converter.py
@@ -2,24 +2,28 @@
 
 """Convert between model coordinates(time, temperature) and view coordinates(pixels, pixels)."""
 
-#from __future__ import division
-#import numpy as np
+import collections as col
 
-def linscale_point(point, inrange, outrange):
-    normalized = (point - inrange[0]) / (inrange[1] - inrange[0])
+def linscale(point, inrange, outrange):
+    normalized = (point - inrange[0]) / float((inrange[1] - inrange[0]))
     scaled = normalized * (outrange[1] - outrange[0])
     shifted = scaled - outrange[0]
     return shifted 
 
-#def linscale_vector(in_vect, out_max):
-#    in_max = np.max(in_vect)
-#    in_norm = in_vect / in_max
-#    scaled = in_norm * out_max
-#    return int(round(scaled))
-
 def temp2pixels(temp, temp_range, graph_height):
-    pix=  linscale_point(temp, temp_range, (0, graph_height))
+    pix = linscale(temp, temp_range, (0, graph_height))
     return int(round(pix))
+
+def seconds2pixels(time, time_range, graph_length):
+    if isinstance(time, col.Iterable):
+        pix = []
+        for t in time:
+            p = linscale(t, time_range, (0, graph_length))
+            pix.append(int(round(p)))
+    else:
+        p = linscale(time, time_range, (0, graph_lenght))
+        pix = int(round(p))
+    return pix
 
 
 def main():
