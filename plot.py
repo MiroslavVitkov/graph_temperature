189ea420937365ca5643d1dd2a7fb6e6ff8b261e
Miroslav Vitkov <sir.vorac@gmail.com>
2013-08-18 23:14:21 +0300

    Plot window now calling the individual graph

--- plot.py
+++ plot.py
@@ -44,49 +44,35 @@ class Window(object):
 
         plt.ion()
 
+        # Create the window space
         dpi=80  # default value
-        size_x = float(plot_width) / dpi
-        size_y = float(plot_height) / dpi
+        size_x = (float(plot_width) / dpi) * plots_num[0]
+        size_y = (float(plot_height) / dpi) * plots_num[1]
         self.fig = plt.figure(figsize=(size_x, size_y), dpi=dpi,
                                        facecolor=None, edgecolor=None,
                                        linewidth=0.0, frameon=None,
                                        subplotpars=None, tight_layout=None
                                       )
-        
-        #self.g = Graph(x_axis=self.x_axis, width=640, height=240)
+
+        # Create the individual plots        
         self.ax = self.fig.add_subplot(111)
         self.line1, = self.ax.plot(self.x_axis, self.x_axis)
+        self.g = Graph(x_axis=self.x_axis, y_axis=self.line1, figure=self.fig)
 
     def update_figure(self, y_pixels):
-        #self.g.update_figure(y_pixels)
-        self.line1.set_ydata(y_pixels)
-        self.fig.canvas.draw()
+        self.g.update_figure(y_pixels)
+        #self.line1.set_ydata(y_pixels)
+        #self.fig.canvas.draw()
 
 class Graph(object):
     """Single 2-D dynamic plot."""
-    def __init__(self, x_axis, width, height):
-        self.x = x_axis
-        self.y = None
-
-        plt.ion()
-
-        dpi=80  # default value
-        size_x = float(width) / dpi
-        size_y = float(height) / dpi
-        self.fig = plt.figure(figsize=(size_x, size_y), dpi=dpi,
-                                       facecolor=None, edgecolor=None,
-                                       linewidth=0.0, frameon=None,
-                                       subplotpars=None, tight_layout=None)
-        self.ax = self.fig.add_subplot(111)
-
-        f = self.fig
-        self.width = f.get_figwidth() * dpi
-        self.height = f.get_figheight() * dpi
-
-        self.line1, = self.ax.plot(self.x, self.x, 'r-') # Returns a tuple of line objects, thus the comma
+    def __init__(self, x_axis, y_axis, figure):
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+        self.fig = figure
 
     def update_figure(self, y_pixels):
-        self.line1.set_ydata(y_pixels)
+        self.y_axis.set_ydata(y_pixels)
         self.fig.canvas.draw()
 
     def get_size(self):
