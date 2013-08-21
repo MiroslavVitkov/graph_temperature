b7eb174a54f9d0e12dac1cb2a8cfe2cd2c39d212
Miroslav Vitkov <sir.vorac@gmail.com>
2013-08-22 00:30:08 +0300

    Flattened the logging i.e. only one massive logfile from now on.

--- data.py
+++ data.py
@@ -10,6 +10,8 @@ X_WEEK = range(0, 60*60*24*7, 60*60*24)
 X_MONTH = range(0, 60*60*24*7*4, 60*60*24*7)
 X_YEAR = range(0, 60*60*24*7*4*12, 60*60*24*7*4)
 
+FNAME_LOG = "temperature_measurements"
+
 class Node(object):
     """General data structuring node. Can be associated with a file in r/rw/w/None way.
 
@@ -31,46 +33,27 @@ class Node(object):
 
 
 class Logger(object):
-    """Records measured data on the disc for later retrieval."""
-    def __init__(self, workdir, backupdir=None):
-        # fname = year_month
-        # self.curr_month
-        # create/open self.curr_file in workdir with name fname
-        # if created, write header
+    """Records measured data on the disc for later retrieval.
+
+    Format is one line per second, appended at a large file.
+    The file is periodically compressed and backed up, with
+    previous archives being deleted.
+    """
+    def __init__(self, workdir, backupdir=None, backup_interval=None):
+        # self.logfile = create/open FNAME_LOG
         pass
 
     def add_line(string):
-        # get system time, extract month
-        # if self.curr_month != month call self.new_month
-        # regardless, write string into self.curr_file
+        # measurement = format string into one line
+        # self.logfile.append(measurement)
         pass
 
-    def begin_new_month(self):
-        """Package away the open logfile for the month,
-        archive it and open a new file.
-
-        """
-        # generate footer: averages per week and for the whole month
-        # close self.curr_file
-        # arcive and save at self.backupdir
-
-    def read_last_minute(self):
-        measurements = []
-        for i in X_MIN:
-            measurements.append(0) # read from file, if end, read from previous
-        return measurements
-
-    def read_last_hour(self)
-        measurements = []
-        for i in range(0, HOUR_TO_SECONDS, MINUTE_TO_SECONDS):
-            measurements.append(0)  # read one line in MINUTE_TO_SAMPLES
-        return measurements
-
-    def read(self, seconds_ago, seconds_increment)
+    def read(self, interval_seconds, step_seconds):
         measurement = []
-        for i in range(0, seconds_ado, seconds_increment):
-            measurements.append(0)  # TODO: read one line from file, if end, read from previous
+        for i in range(0, interval_seconds, step_seconds):
+            measurements.append(0)  # TODO
         return measurement
+
 
 def main():
     pass
