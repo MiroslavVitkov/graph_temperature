f99d6f50193aa39cd5065e46ba27d414e1171640
Miroslav Vitkov <sir.vorac@gmail.com>
2013-09-02 08:05:58 +0300

    Implemented part of serial communication

--- hw_comm.py
+++ hw_comm.py
@@ -4,13 +4,36 @@
 
 """
 
-import pyserail
+import serial
 
 
 class Serial(object):
     """Serial duplex communication."""
     def __init__(self, clb):
-        pass
+        self.clb = clb  # callback for new available measurement
+        self.comm = serial.Serial(port=0,
+                                  baudrate=9600,
+                                  bytesize=EIGHTBITS,
+                                  parity=PARITY_NONE,
+                                  stopbits=STOPBIT_ONE,
+                                  timeout=None,
+                                  xoxoff=False,  # sw flow control
+                                  rtscts=False,  # hw flow control
+                                  writeTimeout=None,
+                                  dsrdtr=False,  # hw flow control
+                                  interCharTimeout=None,
+                                  )
+
+    def run(self):
+        measurement = 30000
+        self.clb(measurement)
 
     def write(self, string, newline=True):
-        pass
+        comm.write(string)
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
